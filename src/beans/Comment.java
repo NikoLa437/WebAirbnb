1156d6116d08cc100ed00801f783ffb5c042c8e3
nikolakolovic7 <nikoladskv@hotmail.rs>
2020-06-23 19:01:02 +0200

    #beans sredjen

--- src/beans/Comment.java
+++ src/beans/Comment.java
@@ -1,8 +1,53 @@
 package beans;
 
 public class Comment {
-	//gost
+	private Guest guest;
 	private Apartment forApartment;
 	private String text;
 	private Grade grade;
+	
+	public Comment() {
+		
+	}
+
+	public Comment(Guest guest, Apartment forApartment, String text, Grade grade) {
+		super();
+		this.guest = guest;
+		this.forApartment = forApartment;
+		this.text = text;
+		this.grade = grade;
+	}
+
+	public Guest getGuest() {
+		return guest;
+	}
+
+	public void setGuest(Guest guest) {
+		this.guest = guest;
+	}
+
+	public Apartment getForApartment() {
+		return forApartment;
+	}
+
+	public void setForApartment(Apartment forApartment) {
+		this.forApartment = forApartment;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public Grade getGrade() {
+		return grade;
+	}
+
+	public void setGrade(Grade grade) {
+		this.grade = grade;
+	}
+
 }
