d56c8d17ee4e5912966c7afe1a3524ff02783d0a
Dusan <dule_srb@hotmail.rs>
2020-06-23 18:14:16 +0200

    asd

---
+++ src/beans/Administrator.java
@@ -0,0 +1,14 @@
+package beans;
+
+public class Administrator extends User {
+
+	public Administrator() {
+		super();
+	}
+	
+	public Administrator(String username, String password, String name, String surname, Gender gender) {
+		super(username, password, name, surname, gender);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
